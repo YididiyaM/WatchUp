9a010fff8f723d4a6bf8f889d0ed5140a0563361
Yididiya Mulatu <yididiyamulatu@gmail.com>
2021-03-02 19:23:16 +0100

    Display fake database on /posts

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
 const express = require('express');
+const { UpgradeRequired } = require('http-errors');
 const app = express();
+const path = require('path');
+const {v4: uuid} = require('uuid');
+
 
 //To parse form data in POST request body:
 app.use(express.urlencoded({ extended: true }))
 // To parse incoming JSON in POST request body:
 app.use(express.json())
 
-app.set('view engine', 'html')
+//Views folder and EJS setup
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
-app.get('/posts', (req, res) => {
+//fake database 
+let posts = [
+    {
+        id: uuid(),
+        username: 'Karen',
+        post: 'There is a homeless man on 52nd secret'
+    },
+
+    {
+        id: uuid(),
+        username: 'Kyle',
+        post: 'Suspicious dude eating hot dogs sideways'
+    }
+]
+// **********************************
+//Render initial html page with form
+// **********************************
+app.get('', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 })
 
-app.post('/posts', (req, res) => {
-    const { meat, qty } = req.body;
-    res.send(`OK, here are your ${qty} ${meat} tacos`)
+app.get('/posts', (req,res)=>{
+    res.render('posts/index', {posts})
 })
 
 app.listen(3001, () => {
